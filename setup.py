d54f1659b2d6bba692b1d5f465484a732d2b635d
icoxfog417 <icoxfog417@yahoo.co.jp>
2018-12-25 16:52:51 +0900

    merge change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="chariot",
-    version="0.4.8",
+    version="0.4.9",
     description="Deliver the ready-to-train data to your NLP model.",
     keywords=["machine learning", "nlp", "natural language processing"],
     author="icoxfog417",
