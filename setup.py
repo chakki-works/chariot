31d85f7d6e43d2fbfcd78e1652073f7d66f8a578
icoxfog417 <icoxfog417@yahoo.co.jp>
2019-10-18 15:22:58 +0900

    remove spacy version fix

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         "numpy>=1.14.4",
         "scipy>=1.1.0",
         "scikit-learn>=0.19.1",
-        "spacy==1.9.0",
+        "spacy>=1.9.0",
         "pandas>=0.23.1",
         "chazutsu>=0.8.2",
         "chakin>=0.0.6",
